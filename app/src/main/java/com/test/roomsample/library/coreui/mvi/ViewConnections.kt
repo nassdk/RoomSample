fb1a41430e8fce56f9fa3b970341ad1c1aba931d
nassdk <nassdk17@gmail.com>
2021-05-03 14:28:30 +0300

    Removed empty lines.

--- app/src/main/java/com/test/roomsample/library/coreui/mvi/ViewConnections.kt
+++ app/src/main/java/com/test/roomsample/library/coreui/mvi/ViewConnections.kt
@@ -1,8 +1,6 @@
 package com.test.roomsample.library.coreui.mvi
 
 interface ViewConnections<State : Any, Intent : Any, Model : Any, Event : Any> {
-
     val stateToModel: (State) -> Model
-
     val eventToIntent: (Event) -> Intent
 }
