c746e0b776b5b9876e24be37984c3429eb8b2044
nassdk <nassdk17@gmail.com>
2021-05-03 18:10:50 +0300

    Removed unused imports.

--- app/src/main/java/com/test/roomsample/feature/flow/di/FlowComponent.kt
+++ app/src/main/java/com/test/roomsample/feature/flow/di/FlowComponent.kt
@@ -1,7 +1,6 @@
 package com.test.roomsample.feature.flow.di
 
 import android.app.Activity
-import com.test.roomsample.feature.flow.DaggerFlowComponent
 import com.test.roomsample.feature.flow.FlowActivity
 import com.test.roomsample.library.coreapi.app.AppProvider
 import com.test.roomsample.library.coreapi.app.AppProviderHolder
